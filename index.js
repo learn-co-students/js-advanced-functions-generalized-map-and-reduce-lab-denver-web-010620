085b84be0e975070f576d3ab95b539442f594dbd
etgreer91 <etgreer91@gmail.com>
2021-05-09 14:56:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+const map = function(arr, func) {
+   let result = [];
+   for(let i = 0; i < arr.length; i++) {
+      result.push(func(arr[i]));    
+   }
+   return result;
+}
+
+const reduce = function(arr, func, start=0) {
+   let result = start;
+   for(let i = 0; i < arr.length; i++) {
+      result = func(arr[i], result);
+      console.log(result);    
+   }
+   return result;
+}
